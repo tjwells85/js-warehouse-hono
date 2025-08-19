fb0019924c18c3789c0b8dd2ebb5b8a2cbc1823d
Tim Wells <tjwells85@outlook.com>
2025-08-19 13:27:52 -0700

    Dependencies

--- server/env.ts
+++ server/env.ts
@@ -7,9 +7,15 @@ export const ServerEnv = z.object({
 	DEV_MODE: z.coerce.boolean().default(false),
 	DATABASE_URL: z.url(),
 	ECLIPSE_URL: z.url(),
+	ECLIPSE_USER: z.string().nonempty({ error: 'Eclipse User cannot be blank' }),
 	ECLIPSE_PASS: z.string().nonempty({ error: 'Eclipse Password cannot be blank' }),
 	ORIGIN: z.url(),
 	AUTH_SECRET: z.string().nonempty({ error: 'Auth Secret cannot be blank' }),
+	PASSKEY_RP_ID: z.string().nonempty({ error: 'Passkey RP ID cannot be blank' }),
+	PASSKEY_ORIGIN: z.url().nonempty({ error: 'Passkey Origin cannot be blank' }),
+	DEFAULT_ADMIN_EMAIL: z.email().nonempty({ error: 'Default Admin Email cannot be blank' }),
+	DEFAULT_ADMIN_PASSWORD: z.string().min(8).nonempty({ error: 'Default Admin Password cannot be blank' }),
+	DEFAULT_ADMIN_NAME: z.string().default('Administrator'),
 });
 export type ServerEnv = z.infer<typeof ServerEnv>;
 
