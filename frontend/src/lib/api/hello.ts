8b0a9d6a32d5ecf008dbf3c5fed3ed204b740b3d
Tim Wells <tjwells85@outlook.com>
2025-08-19 13:49:43 -0700

    deleted example route

--- frontend/src/lib/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { apiClient } from './client';
-
-export const hello = async () => {
-	const res = await apiClient.hello.$get();
-	const { message } = await res.json();
-
-	return `${message} from the server!`;
-};
