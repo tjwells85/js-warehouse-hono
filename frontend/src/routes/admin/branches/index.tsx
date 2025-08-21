3b887cb544ff71ba08af286fe205810ea1bcbfd0
Tim Wells <tjwells85@outlook.com>
2025-08-21 16:02:14 -0700

    branches admin page

--- frontend/src/routes/admin/branches/index.tsx
+++ frontend/src/routes/admin/branches/index.tsx
@@ -1,9 +1,47 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { getAllBranches } from '@frontend/lib/api/branch';
+import Title from '@frontend/components/Title';
+import { isFailure } from '@frontend/lib/types';
 
 export const Route = createFileRoute('/admin/branches/')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+	loader: async () => {
+		const res = await getAllBranches();
+		if (isFailure(res)) {
+			throw new Error(JSON.stringify(res), { cause: res.error });
+		}
+		return { branches: res.data };
+	},
+});
 
 function RouteComponent() {
-  return <div>Hello "/admin/branches/"!</div>
+	const { branches } = Route.useLoaderData();
+
+	return (
+		<main>
+			<Title>Branches</Title>
+			<table className="table table-zebra mx-auto w-[15%] rounded-lg bg-base-300 shadow-xl">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Store #</th>
+						<th>Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					{branches.map((br) => (
+						<tr key={br._id}>
+							<td>{br.brId}</td>
+							<td>{br.num}</td>
+							<td>
+								<Link to="/admin/branches/$id" params={{ id: br.brId }} className="link font-semibold text-blue-500">
+									{br.name}
+								</Link>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</main>
+	);
 }
